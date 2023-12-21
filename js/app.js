a7d30c707ca2d236f6fe551afe0167037857b43f
Kaique Gonçalves <kaique121106@gmail.com>
2023-12-20 23:05:58 -0300

    selecionando os botões de alugar e devolver

---
+++ js/app.js
@@ -0,0 +1,2 @@
+const listaDeBotoes = [document.querySelector('#game-1').querySelector('.dashboard__item__button'), document.querySelector('#game-2').querySelector('.dashboard__item__button'), document.querySelector('#game-3').querySelector('.dashboard__item__button')];
+ 
