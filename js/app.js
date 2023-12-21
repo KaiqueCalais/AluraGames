6f902e59911e713d662280285c8d69028636b3c2
Kaique Gonçalves <kaique121106@gmail.com>
2023-12-20 23:42:54 -0300

    criando uma lista dos jogos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
-const listaDeBotoes = [document.querySelector('#game-1').querySelector('.dashboard__item__button'), document.querySelector('#game-2').querySelector('.dashboard__item__button'), document.querySelector('#game-3').querySelector('.dashboard__item__button')];
+const listaDeJogos = [document.querySelector('#game-1'), document.querySelector('#game-2'), document.querySelector('#game-3')];
+
+const listaDeBotoes = [listaDeJogos[0].querySelector('.dashboard__item__button'), listaDeJogos[1].querySelector('.dashboard__item__button'), listaDeJogos[2].querySelector('.dashboard__item__button')];
  
+
